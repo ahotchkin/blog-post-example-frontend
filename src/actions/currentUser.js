240958cba2e40246c0f4862ffeac80fef9b19ef2
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-08 16:14:31 -0500

    Add signup action to create user on the backend when user submits signup form

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -13,6 +13,33 @@ export const clearCurrentUser = () => {
 }
 
 // asynchronous actions
+export const signUp = credentials => {
+  console.log("credentials are: ", credentials)
+  const userInfo = {
+    user: credentials
+  }
+  return dispatch => {
+    return fetch("http://localhost:3001/signup", {
+      credentials: "include",
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(userInfo)
+    })
+      .then(response => response.json())
+      .then(json => {
+        if (json.error) {
+          throw new Error(json.error)
+        } else {
+          console.log(json)
+          dispatch(setCurrentUser(json))
+        }
+      })
+      .catch(json => console.log(json))
+  }
+}
+
 export const login = credentials => {
   console.log("credentials are: ", credentials)
   return dispatch => {
