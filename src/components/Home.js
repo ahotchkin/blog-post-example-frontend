7ad9c30068f22e056d52c0eb9e9ebbba67f00fcd
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-24 14:16:32 -0500

    Remove method not being used, remove unnecessary comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,20 +8,10 @@ import { logout } from '../actions/currentUser';
 
 const Home = props => {
 
-  const handleOnSubmit = event => {
-    event.preventDefault()
-    props.logout()
-  }
-
   return (
     <div>
       <h1>Welcome to your Home Page, {props.currentUser.attributes.username}!</h1>
       <Table />
-      {/*
-      <form onSubmit={handleOnSubmit}>
-        <input type="submit" value="Log Out" />
-      </form>
-      */}
       <Link to="/logout" onClick={props.logout}>
         Log Out
       </Link>
