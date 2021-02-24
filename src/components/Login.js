ce9c52504cb0daec03323c8710ff76876e51d21c
ahotchkin <allyson.hotchkin@gmail.com>
2021-02-24 14:17:21 -0500

    Import UserInfoForm, move rendered form to separate component and render component, update to functional component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 
+import UserInfoForm from './UserInfoForm'
 import { login } from '../actions/currentUser';
 
-class Login extends Component {
-
-  state = {
-    username: "",
-    password: ""
-  }
-
-  handleOnChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  handleOnSubmit = event => {
-    event.preventDefault();
-    this.props.login(this.state)
-    this.setState({
-      username: "",
-      password: ""
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Log In Here!</h1>
-        <form onSubmit={this.handleOnSubmit}>
-          <label htmlFor="username">Username: </label>
-          <input
-            type="text"
-            name="username"
-            id="username"
-            value={this.state.username}
-            onChange={this.handleOnChange}
-          />
-
-          <br />
-
-          <label htmlFor="password">Password: </label>
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={this.state.password}
-            onChange={this.handleOnChange}
-          />
-
-          <br />
-
-          <input type="submit" value="Log In" />
-        </form>
-      </div>
-    )
-  }
+const Login = props => {
+  return (
+    <div>
+      <h1>Log In Here!</h1>
+      <UserInfoForm buttonText="Log In" onFormSubmit={props.login} />
+    </div>
+  )
 }
 
-const mapDispatchToProps = {
-  login
-}
-
-export default connect(null, mapDispatchToProps)(Login);
+export default connect(null, { login })(Login);
